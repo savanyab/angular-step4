e3524562e527024a75d28d87018e20b7f6e88e46
savanyab <savanyab@gmail.com>
2018-12-14 10:22:58 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TaskListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/tasks/task-list/task-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+  tasks = ['Cook', 'Do laundry', 'Clean up', 'Walk the dog'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getBgColor(i) {
+    if (i === 0) {
+      return "pink";
+    }
+  }
+
+  getBold(i) {
+    if (i % 2 !== 0) {
+      return 'bold';
+    }
+  }
+
+}
